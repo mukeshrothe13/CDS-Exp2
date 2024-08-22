4cae24751c8639f351e42ae1121800628b7a658f
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 20:46:40 +0530

    Update 2B_static.cpp

--- 2B_static.cpp
+++ 2B_static.cpp
@@ -1,13 +1,13 @@
-// Experiment 2
-#include <iostream>
+//Mukesh Rothe //23070123089 //CDS EXP2
+#include<iostream>
 using namespace std;
 void staticExample(){
-    static int z=0;
-    z++;
-    cout<<"The Value of z is:"<<z<<endl;
+int z = 0;
+z++;
+cout<<"the value of z is: "<<z<<endl;
 }
-int main() {
-    staticExample();
-    staticExample();
-    return 0;
+int main(){
+staticExample();
+staticExample();
+return 0;
 }
