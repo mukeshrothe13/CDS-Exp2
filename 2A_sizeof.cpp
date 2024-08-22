04ca604ca34b1f43937e4da6c90448b99b085ecd
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 20:45:42 +0530

    2A_sizeof.cpp

--- 2A_sizeof.cpp
+++ 2A_sizeof.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
-int main(){
-    char a[100];
-    cout<<"size of int "<<sizeof(int)<<" char "<<sizeof(char)<<" float "<<sizeof(float)
-    <<" double "<<sizeof(string)<<" long "<<sizeof(bool)<<" array "<<sizeof(a);
-    return 0;
+int main()
+{ 
+cout << "Size of Integer is: "   << sizeof(int) <<" bytes"<< endl;
+cout << "Size of Float is: "   << sizeof(float) <<" bytes"<< endl;
+cout << "Size of String is: "   << sizeof(string) <<" bytes"<< endl;
+cout << "Size of Character is: "   << sizeof(char) <<" bytes"<< endl;
+cout << "Size of Character is: "   << sizeof(char) <<" bytes"<< endl;
+cout << "Size of Short Integer is: "   << sizeof(short int) <<" bytes"<< endl;
 }
