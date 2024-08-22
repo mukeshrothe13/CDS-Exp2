90215742da1dd5db0dfbf9fbf40d9157e64a56c1
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 20:56:20 +0530

    2A_sizeof.cpp

--- 2A_sizeof.cpp
+++ 2A_sizeof.cpp
@@ -7,6 +7,5 @@ cout << "Size of Integer is: "   << sizeof(int) <<" bytes"<< endl;
 cout << "Size of Float is: "   << sizeof(float) <<" bytes"<< endl;
 cout << "Size of String is: "   << sizeof(string) <<" bytes"<< endl;
 cout << "Size of Character is: "   << sizeof(char) <<" bytes"<< endl;
-cout << "Size of Character is: "   << sizeof(char) <<" bytes"<< endl;
 cout << "Size of Short Integer is: "   << sizeof(short int) <<" bytes"<< endl;
 }
